95fb202dd30a22bb35577a655391c2d2a023a85c
Sebastian Mazgaj <rementai1@gmail.com>
2023-06-10 21:41:09 +0200

    lab10

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import Posts from "./components/posts"
+import Home from "./components/home"
+import Login from "./components/loginForm"
+import NotFound from './components/notFound'
+import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+      <BrowserRouter>
+          <Routes>
+                <Route path="/" element={<App/>}>
+                  
+                <Route path="home" element={<Home/>}/>
+
+                <Route path="posts" element={<Posts/>}/>
+
+                <Route path="login" element={<Login/>}/>
+
+                <Route path="*" element={<NotFound/>}/>
+              </Route>
+          </Routes>
+      </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
